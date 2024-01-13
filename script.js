25bcb68dbc7c1cdb5aafaf4108a988dcadd38eaf
Jasper Mawunyo Kojo Akabua <jasperakabua020@gmail.com>
2024-01-13 18:14:35 +0000

    Script file added, menu action added

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu");
+const navbar = document.querySelector("#left-nav");
+
+    menu.addEventListener('click', () =>{
+        // menu.classList.toggle('bx-l');
+    navbar.classList.toggle('active');
+    });
